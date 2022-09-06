ad4244af4263e7572541dc1e22792fd5590118b0
Faizan Ali <34148227+faizan-official@users.noreply.github.com>
2022-09-07 01:09:40 +0500

    made env var all caps

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import './App.css';
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apiKey=" + process.env.REACT_APP_OMDBAPI_API_KEY;
+const API_URL = "https://www.omdbapi.com?apiKey=" + process.env.REACT_APP_OMDBAPI_API_KEY;
 const App = () => {
 
   const [movies, setMovies] = useState([]);
